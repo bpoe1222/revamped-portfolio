787d51ac1446d2e6e37e6117fa1f48c6e574479a
bpoe1222 <baileypoe1222@gmail.com>
2024-03-29 15:41:20 -0500

    Added a little CSS

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <div className="gradient-bg">
+      <div className='gradients-container'>
+        <div className='g1'></div>
+        <div className='g2'></div>
+        <div className='g3'></div>
+        <div className='g4'></div>
+        <div className='g5'></div>
+      </div>
     </div>
+    <div className='super-big'>test</div>
+    </div>
+    
   );
 }
 
